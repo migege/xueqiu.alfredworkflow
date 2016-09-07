b7a1470b6154dc484d01ebffa794d7757074e8c9
lzw.whu <lzw.whu@gmail.com>
2016-09-07 08:31:40 +0800

    first commit

---
+++ xueqiu.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+import sys
+import os
+import requests
+from alfred.feedback import Feedback
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def run(q):
+    q = q.strip()
+    if not q:
+        return
+
+    try:
+        url = 'https://xueqiu.com/v4/stock/quotec.json?code=%s' % q
+        headers = {
+            'Host': 'xueqiu.com',
+            'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36',
+        }
+        r = requests.get(url, headers=headers)
+        res = r.json()
+        data = res[q]
+        price = data[0]
+        delta = data[1]
+        rate = data[2]
+        time = data[3]
+        title = '当前价:{} 涨:{} 涨幅:{}'.format(price, delta, rate)
+        link = 'https://xueqiu.com/S/%s' % q
+        kwargs = {'title': title, 'subtitle': title, 'arg': link}
+
+        fb = Feedback()
+        fb.addItem(**kwargs)
+        fb.output()
+    except Exception, e:
+        print 'EXCEPT:', e
+        pass
+
+
+if __name__ == '__main__':
+    q = '{query}'
+    run(q)
