9411fba624f6889b25386ae50aaececd9f1b010c
zwli <zwli@hillinsight.com>
2020-05-07 17:47:20 +0800

    update

--- xueqiu.py
+++ xueqiu.py
@@ -15,20 +15,10 @@ def run(q, xq_a_token):
         return
 
     try:
-        url = 'https://xueqiu.com/v4/stock/quotec.json?code=%s' % q
         headers = {
             'Host': 'xueqiu.com',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36',
         }
-        r = requests.get(url, headers=headers)
-        res = r.json()
-        data = res[q]
-        price = data[0]
-        delta = data[1]
-        rate = data[2]
-        time = data[3]
-        title = '当前价:{} 涨:{} 涨幅:{}%'.format(price, delta, rate)
-        link = 'https://xueqiu.com/S/%s' % q
 
         url = 'https://xueqiu.com/v4/stock/quote.json?code=%s' % q
         cookies = {
@@ -37,6 +27,8 @@ def run(q, xq_a_token):
         r = requests.get(url, headers=headers, cookies=cookies)
         res = r.json()
         data = res[q]
+        title = '当前价:{current} 涨:{change} 涨幅:{percentage}%'.format(**data)
+        link = 'https://xueqiu.com/S/%s' % q
         subtitle = '{name} 昨收:{last_close} 今开:{open} 最高:{high} 最低:{low}'.format(**data)
 
         kwargs = {'title': title, 'subtitle': subtitle, 'arg': link}
